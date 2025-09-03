1a06da8f779d9553275a22c1352a19238ea5a876
Mikhail Tsekhanovich <mikhail.m1@bk.ru>
2025-09-03 17:20:12 +0400

    git init

---
+++ src/main/java/ru/nicetu/crtris/crtrisbackend/CrtrisBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.nicetu.crtris.crtrisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrtrisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrtrisBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/nicetu/crtris/crtrisbackend/CrtrisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nicetu.crtris.crtrisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrtrisBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
